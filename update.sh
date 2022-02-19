b580ba08b39b59417b50b303994c9fc289699b6e
Chuzs <291822616@qq.com>
2022-02-19 04:06:00 +0000

    update checkData

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-rm /home/chuzs/Github/Chuzs.github.io/checkData/* -rf
-unzip /storage/internal/dist.zip -d /home/chuzs/Github/Chuzs.github.io/checkData
+rm /home/chuzs/Github/Chuzs.github.io/checkData/* -r
+unzip /storage/internal/*dist.zip -d /home/chuzs/Github/Chuzs.github.io/checkData
 git add .
 git commit -m "update checkData"
 git push
